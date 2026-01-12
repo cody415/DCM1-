84c35f0caff72d87220ce5814190bff341fbd00d
cody415 <aparnadesai65@gmail.com>
2026-01-12 10:13:45 +0530

    Create string_operation.py

---
+++ string_operation.py
@@ -0,0 +1,15 @@
+first_name = "Aparna"
+last_name = "Patel"
+full_name = first_name + " " + last_name
+repeat_name = first_name * 3
+
+print(first_name)
+print(last_name)
+print(full_name)
+print(repeat_name)
+
+random_string = "PythonProgramming"
+print("Length:", len(random_string))
+print("First character:", random_string[0])
+print("Last character:", random_string[-1])
+print("Substring (2nd to 6th):", random_string[1:6])
