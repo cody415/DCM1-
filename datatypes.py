66a8167c8b5bc41b9f3678f8bc03b5148ce86b5a
cody415 <aparnadesai65@gmail.com>
2026-01-12 10:11:23 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,15 @@
+name = "Aparna"
+age = 22
+weight = 55.8
+is_student = True
+
+print("Data type of name:", type(name))
+print("Data type of age:", type(age))
+print("Data type of weight:", type(weight))
+print("Data type of is_student:", type(is_student))
+
+age = str(age)
+weight = int(weight)
+
+print("New data type of age:", type(age))
+print("New data type of weight:", type(weight))
