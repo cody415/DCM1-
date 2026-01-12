89f9e1f350d3d6829434ad31b4f56b33513539e9
cody415 <aparnadesai65@gmail.com>
2026-01-12 10:14:46 +0530

    Create Number_swap.py

---
+++ Number_swap.py
@@ -0,0 +1,8 @@
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+print("Before swapping: x =", x, ", y =", y)
+
+x, y = y, x
+
+print("After swapping: x =", x, ", y =", y)
