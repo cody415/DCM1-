0456813e08d4cd7d62c2a20ef11e702f4929fc70
cody415 <aparnadesai65@gmail.com>
2026-01-12 09:32:13 +0530

    Create printing.py

---
+++ printing.py
@@ -0,0 +1,8 @@
+# This line prints a greeting message
+print("Hello, Python learners!")
+
+# This line prints the result of a calculation
+print(5 + 3)   # Addition
+print(10 - 4)  # Subtraction
+print(6 * 2)   # Multiplication
+print(8 / 2)   # Division
